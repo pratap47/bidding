38ba838ff60c4295185006be444d1642329895db
pratap47 <agrawal.akash407@gmail.com>
2019-02-05 03:02:56 +0530

    hosts

---
+++ app/src/main/java/com/bullseye/bidding/adapters/MyBidAdapter.java
@@ -0,0 +1,58 @@
+package com.bullseye.bidding.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bullseye.bidding.R;
+import com.bullseye.bidding.bid.My_host_listitems;
+
+import java.util.List;
+
+public class MyBidAdapter extends RecyclerView.Adapter<MyBidAdapter.ViewHolder> {
+
+    private List<My_host_listitems> listitems;
+    private Context context;
+
+    public MyBidAdapter(List<My_host_listitems> listitems, Context context) {
+        this.listitems = listitems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyBidAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_host_listitems,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyBidAdapter.ViewHolder viewHolder, int position) {
+        My_host_listitems l = listitems.get(position);
+        viewHolder.txtDescrip.setText(l.getmDescrip());
+        viewHolder.txtName.setText(l.getmName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView txtName;
+        public TextView txtDescrip;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtName = (TextView)itemView.findViewById(R.id.txtName);
+            txtDescrip =(TextView)itemView.findViewById(R.id.txtDescrip);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/bullseye/bidding/bid/My_host_listitems.java
@@ -0,0 +1,30 @@
+package com.bullseye.bidding.bid;
+
+public class My_host_listitems {
+
+    private String mName;
+    private String mDescrip;
+
+
+    public My_host_listitems(String mName, String mDescrip) {
+        this.mName = mName;
+        this.mDescrip = mDescrip;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmDescrip() {
+        return mDescrip;
+    }
+
+    public void setmDescrip(String mDescrip) {
+        this.mDescrip = mDescrip;
+    }
+}
+
